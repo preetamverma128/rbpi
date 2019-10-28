4bb1f0a457b4fea3ccf824814855388c3ad36240
Preetam Verma <preetam.verma@gmail.com>
2019-10-28 19:45:38 +0530

    initiate meet.jist

---
+++ video_call.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+chromium-browser  --disable-session-crashed-bubble  --disable-restore-session-state https://meet.jit.si/preetamv128128
+
+
+#chromium-browser --kiosk --disable-session-crashed-bubble --disable-infobars --disable-restore-session-state https://meet.jit.si/preetamv12128
